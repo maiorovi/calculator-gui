4060edb75bc7979b196f1a01acbe34c9e1fcdd23
maiorovi <maiorov.prog@gmail.com>
2017-09-17 13:00:40 +0300

    added more digits and handler with two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CalculatorCoreComponent } from './calculator-core/calculator-core.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { CalculatorCoreComponent } from './calculator-core/calculator-core.compo
     CalculatorCoreComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/calculator-core/calculator-core.component.ts
+++ src/app/calculator-core/calculator-core.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator-core.component.css']
 })
 export class CalculatorCoreComponent implements OnInit {
-
+  expression:String = '';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  extendExpressionWith(digit) {
+    this.expression += digit;
+  }
+
+  clearExpression() {
+    this.expression = '';
+  }
+
+  clearLast() {
+    this.expression = this.expression.substring(0, this.expression.length - 1)
   }
 
 }
